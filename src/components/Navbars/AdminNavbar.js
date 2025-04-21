41ab1c707e1dfb235aa79a7f4d6e70def1e979d6
Jazim Nazir <109747177+Jazimnazir@users.noreply.github.com>
2025-04-21 12:33:29 +0530

    navbar
    changes

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -1,87 +1,78 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React, { Component } from "react";
-import { useLocation } from "react-router-dom";
-import { Navbar, Container, Nav, Dropdown, Button } from "react-bootstrap";
-
-import routes from "routes.js";
+import React from "react";
+import { Navbar, Nav, Container, Image } from "react-bootstrap";
+import reactlogo from "../../assets/img/reactlogo.png";
 
 function Header() {
-  const location = useLocation();
-  const mobileSidebarToggle = (e) => {
-    e.preventDefault();
-    document.documentElement.classList.toggle("nav-open");
-    var node = document.createElement("div");
-    node.id = "bodyClick";
-    node.onclick = function () {
-      this.parentElement.removeChild(this);
-      document.documentElement.classList.toggle("nav-open");
-    };
-    document.body.appendChild(node);
-  };
-
-  const getBrandText = () => {
-    for (let i = 0; i < routes.length; i++) {
-      if (location.pathname.indexOf(routes[i].layout + routes[i].path) !== -1) {
-        return routes[i].name;
-      }
-    }
-    return "Brand";
-  };
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar
+      expand="lg"
+      style={{
+        backgroundColor: "#0e2288",
+        padding: "1rem",
+      }}
+      variant="dark"
+    >
       <Container fluid>
-        <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
-          <Button
-            variant="dark"
-            className="d-lg-none btn-fill d-flex justify-content-center align-items-center rounded-circle p-2"
-            onClick={mobileSidebarToggle}
-          >
-            <i className="fas fa-ellipsis-v"></i>
-          </Button>
-          <Navbar.Brand
-            href="#home"
-            onClick={(e) => e.preventDefault()}
-            className="mr-2"
-          >
-            {getBrandText()}
-          </Navbar.Brand>
-        </div>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-2">
-          <span className="navbar-toggler-bar burger-lines"></span>
-          <span className="navbar-toggler-bar burger-lines"></span>
-          <span className="navbar-toggler-bar burger-lines"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="nav mr-auto " navbar>
+        <div className="d-flex flex-column align-items-start">
+          {/* Top Row: Logo + Title */}
+          <div className="d-flex align-items-center mb-2">
+            <Navbar.Brand href="#" onClick={(e) => e.preventDefault()}>
+              <Image
+                src={reactlogo}
+                alt="Logo"
+                style={{ width: "70px", height: "auto" }}
+              />
+            </Navbar.Brand>
+
+            <div style={{ marginLeft: "10px" }}>
+              <span
+                style={{
+                  fontSize: "1.2rem",
+                  fontWeight: "500",
+                  color: "#FFD700",
+                }}
+              >
+                SCHEDULE MANAGEMENT SOFTWARE
+              </span>
+            </div>
+          </div>
+
+          {/* Bottom Row: Nav Links */}
+          <Nav className="d-flex align-items-center">
             <Nav.Item>
               <Nav.Link
-                data-toggle="dropdown"
-                href="#pablo"
+                href="#"
                 onClick={(e) => e.preventDefault()}
-                className="m-0"
+                className="text-white"
+                style={{ fontSize: "1.2rem", fontWeight: "500" }}
               >
-                
-                <span className="no-icon" >Log out</span>
+                Dashboard
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                href="#"
+                onClick={(e) => e.preventDefault()}
+                className="text-white"
+                style={{ fontSize: "1.2rem", fontWeight: "500" }}
+              >
+                User
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                href="#"
+                onClick={(e) => e.preventDefault()}
+                className="text-white"
+                style={{ fontSize: "1.2rem", fontWeight: "500" }}
+              >
+                Company
               </Nav.Link>
             </Nav.Item>
           </Nav>
-        </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
