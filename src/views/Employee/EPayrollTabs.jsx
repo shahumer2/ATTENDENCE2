831f3db38837aca452ce16e57a7e35f64f81f28a
UMER MUKHTAR <shah.umer8493@gmail.com>
2025-08-01 09:51:29 +0530

    e

--- src/views/Employee/EPayrollTabs.jsx
+++ src/views/Employee/EPayrollTabs.jsx
@@ -510,7 +510,11 @@ const ePayrollDTOTabs = ({ values, setFieldValue, appDetails, setAppDetails }) =
                     {tabs.map((tab) => (
                         <button
                             key={tab.id}
-                            onClick={() => setActiveTab(tab.id)}
+                            onClick={(e) =>{
+                                e.preventDefault();
+                             setActiveTab(tab.id)}}
+
+                                
                             className={`whitespace-nowrap py-4 px-6 border-b-2 font-medium text-sm ${activeTab === tab.id
                                 ? 'border-blue-500 text-blue-600'
                                 : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
